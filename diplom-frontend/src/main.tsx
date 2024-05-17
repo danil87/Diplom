5682d466e4ccd85433571faf39f28c7a44ebed13
danil87 <kokovin.danil@list.ru>
2024-05-17 23:53:25 +0300

    Настройка фронтенда

--- diplom-frontend/src/main.tsx
+++ diplom-frontend/src/main.tsx
@@ -1,10 +1,21 @@
+import './index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+
+import { App } from './App.tsx'
+import { createStore } from './store'
+
+const store = createStore()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 )
